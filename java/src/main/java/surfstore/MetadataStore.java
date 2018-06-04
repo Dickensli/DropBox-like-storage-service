a62b53b26eb55722b718ce88c142b80920e09799
yagou116 <yad039@eng.ucsd.edu>
2018-06-04 11:09:28 -0700

    MetadataStore (v1) complete

--- java/src/main/java/surfstore/MetadataStore.java
+++ java/src/main/java/surfstore/MetadataStore.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -22,24 +23,80 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import surfstore.SurfStoreBasic.*;
 
 public final class MetadataStore {
-    private static final Logger logger = Logger.getLogger(MetadataStore.class.getName());
+	private static final Logger logger = Logger.getLogger(MetadataStore.class.getName());
 
-    private final ManagedChannel blockChannel;
-    private static BlockStoreGrpc.BlockStoreBlockingStub blockStub;
+	private final ManagedChannel blockChannel;
+	private static BlockStoreGrpc.BlockStoreBlockingStub blockStub;
+	private final ArrayList<ManagedChannel> metadataChannelList;
+	private static ArrayList<MetadataStoreGrpc.MetadataStoreBlockingStub> metadataStubList;
+	private static ArrayList<HashMap<String, Integer>> followerMapList;
+	private static ArrayList<surfstore.SurfStoreBasic.FileInfo> updatedFiles;
 
-    protected Server server;
-    private static ConfigReader config;
-    private static boolean leader;
-    private static boolean crashed;
+	protected Server server;
+	private static ConfigReader config;
+	private static boolean leader;
+	private static boolean crashed;
 
-    public MetadataStore(ConfigReader config, Namespace c_args) {
-    	this.blockChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getBlockPort())
-                .usePlaintext(true).build();
-        this.blockStub = BlockStoreGrpc.newBlockingStub(blockChannel);
+	public MetadataStore(ConfigReader config, Namespace c_args) {
+        	this.config = config;
+        	this.leader = config.getLeaderNum() == c_args.getInt("number");
+        	this.crashed = false;
 
-	this.config = config;
-	this.leader = config.getLeaderNum() == c_args.getInt("number");
-	this.crashed = false;
+		this.blockChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getBlockPort()).usePlaintext(true).build();
+                this.blockStub = BlockStoreGrpc.newBlockingStub(blockChannel);
+		this.metadataChannelList = new ArrayList<ManagedChannel>();
+                this.metadataStubList = new ArrayList<MetadataStoreGrpc.MetadataStoreBlockingStub>();
+                this.followerMapList = new ArrayList<HashMap<String, Integer>>();
+		this.updatedFiles = new ArrayList<surfstore.SurfStoreBasic.FileInfo>();
+
+        	// leader
+		if (this.leader) {
+                        for (int i = 1; i <= config.getNumMetadataServers(); i++){
+				if (i != config.getLeaderNum()){
+					ManagedChannel metadataChannel = ManagedChannelBuilder.forAddress("127.0.0.1", config.getMetadataPort(i)).usePlaintext(true).build();
+					MetadataStoreGrpc.MetadataStoreBlockingStub metadataStub = MetadataStoreGrpc.newBlockingStub(metadataChannel);
+					this.metadataChannelList.add(metadataChannel);
+					this.metadataStubList.add(metadataStub);
+					HashMap<String, Integer> followerMap = new HashMap<String, Integer>();
+					this.followerMapList.add(followerMap);
+				}
+			}
+		}	
+	}
+	// update followers during heartbeat
+	private int update(surfstore.SurfStoreBasic.FileInfo request) {
+                int updated = 0;
+                for (int i = 0; i < metadataStubList.size(); i++) {
+                        MetadataStoreGrpc.MetadataStoreBlockingStub metaStub = metadataStubList.get(i);
+                        metaStub.ping(Empty.newBuilder().build());
+                        // 1st phase
+                        if (metaStub.isCrashed(Empty.newBuilder().build()).getAnswer() == false) { // not crashed
+				// 2nd phase
+				if (metaStub.updateFollower(request).getResult() == WriteResult.Result.OK) {
+					followerMapList.get(i).put(request.getFilename(), request.getVersion());
+					updated++;
+				}
+			}
+		}
+		return updated;
+	}
+	
+	private void heartbeat() {
+		while (true) {
+			try { // sleep for 500ms
+				Thread.sleep(500);
+			}
+			catch (InterruptedException e) {System.err.println(e);}
+			Iterator<surfstore.SurfStoreBasic.FileInfo> i = updatedFiles.iterator();
+			while (i.hasNext()) {
+   				surfstore.SurfStoreBasic.FileInfo file = i.next(); 
+   				// if all the followers are updated, remove file from updatedFiles
+				int success = update(file);
+				if (success + 1 == config.getNumMetadataServers()){
+   					i.remove();
+				}
+   			}
+		}
 	}
 
 	private void start(int port, int numThreads) throws IOException {
@@ -57,6 +114,21 @@ public final class MetadataStore {
                 System.err.println("*** server shut down");
             }
         });
+
+	// leader heartbeat
+	if (leader) {
+		Thread heartbeatThread = 
+		    new Thread(
+			new Runnable() {
+			    @Override
+			    public void run() {
+				heartbeat();	
+			    }
+			});
+		heartbeatThread.setName("grpc-heartbeat");
+		heartbeatThread.setDaemon(true);
+		heartbeatThread.start();
+	}
     }
 
     private void stop() {
@@ -112,6 +184,7 @@ public final class MetadataStore {
 	protected Map<String, ArrayList<String>> metadataMap;
 	protected Map<String, Integer> versionMap;
 	
+
 	public MetadataStoreImpl(){
 		super();
 		this.metadataMap = new HashMap<String, ArrayList<String>>();
@@ -169,6 +242,47 @@ public final class MetadataStore {
 		return misslist;
 	}
 
+	// 2nd phase 
+        @Override
+	public void updateFollower(surfstore.SurfStoreBasic.FileInfo request,
+        io.grpc.stub.StreamObserver<surfstore.SurfStoreBasic.WriteResult> responseObserver) {
+		
+		WriteResult.Builder builder = WriteResult.newBuilder();
+
+		if (!crashed){
+			logger.info("Updating file " + request.getFilename());
+		
+			versionMap.put(request.getFilename(), request.getVersion());
+			ArrayList<String> hashlist = new ArrayList<String>(request.getBlocklistList());
+			metadataMap.put(request.getFilename(), hashlist);
+			builder.setResult(WriteResult.Result.OK);
+		} else {
+			logger.info("Crashed during updating file " + request.getFilename());
+			builder.setResult(WriteResult.Result.OLD_VERSION);
+		}
+
+		WriteResult response = builder.build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+	}
+
+	private int twoPhase(surfstore.SurfStoreBasic.FileInfo request) {
+		int updated = 0;
+		for (int i = 0; i < metadataStubList.size(); i++) {
+			MetadataStoreGrpc.MetadataStoreBlockingStub metaStub = metadataStubList.get(i);
+			metaStub.ping(Empty.newBuilder().build());
+			// 1st phase
+			if (metaStub.isCrashed(Empty.newBuilder().build()).getAnswer() == false) { // not crashed
+				// 2nd phase
+				if (metaStub.updateFollower(request).getResult() == WriteResult.Result.OK) {
+					followerMapList.get(i).put(request.getFilename(), request.getVersion());
+					updated++;
+				}
+			}
+		}
+		return updated;
+	}
+	
 	@Override
     	public void modifyFile(surfstore.SurfStoreBasic.FileInfo request,
         	io.grpc.stub.StreamObserver<surfstore.SurfStoreBasic.WriteResult> responseObserver) {
@@ -190,12 +304,22 @@ public final class MetadataStore {
 					builder.setResult(WriteResult.Result.MISSING_BLOCKS);
                 			builder.setCurrentVersion(cur_version);
 					builder.addAllMissingBlocks(iter_misslist); /* check if addAll repeated field workds */
-				} else { // success
-					// update maps if no missing block
-					versionMap.put(request.getFilename(),  request.getVersion());
-                        		metadataMap.put(request.getFilename(), hashlist);
-					builder.setResult(WriteResult.Result.OK);
-					builder.setCurrentVersion(request.getVersion());
+				} else { // no missing blocks
+					// update followers - success if more than half of servers are updated
+					int twoPCsuccess = twoPhase(request);
+					if (2 * (twoPCsuccess + 1) > config.getNumMetadataServers()){
+						versionMap.put(request.getFilename(),  request.getVersion());
+                                        	metadataMap.put(request.getFilename(), hashlist);
+						if (twoPCsuccess + 1 < config.getNumMetadataServers()){
+                                        		// not all followers updated
+							updatedFiles.add(request);
+						}
+						builder.setResult(WriteResult.Result.OK);
+                                        	builder.setCurrentVersion(request.getVersion());
+					} else { // more than half of servers crashed
+						builder.setResult(WriteResult.Result.OLD_VERSION);
+						builder.setCurrentVersion(cur_version);
+					}
 				}
 			} else { /* not valid */
 				builder.setResult(WriteResult.Result.OLD_VERSION);
@@ -221,13 +345,24 @@ public final class MetadataStore {
 
                 	int cur_version = versionMap.get(request.getFilename());
 			if (request.getVersion() == cur_version + 1) { /* valid */
-				// update maps
-				ArrayList<String> hashlist = new ArrayList<String>();
-				versionMap.put(request.getFilename(), request.getVersion());
-				metadataMap.put(request.getFilename(), hashlist);
-				
-				builder.setResult(WriteResult.Result.OK);
-				builder.setCurrentVersion(request.getVersion());
+				int twoPCsuccess = twoPhase(request);
+				if (2 * (twoPCsuccess + 1) > config.getNumMetadataServers()){ 
+					// update maps
+					ArrayList<String> hashlist = new ArrayList<String>();
+					String hash = "0";
+					hashlist.add(hash);
+					versionMap.put(request.getFilename(), request.getVersion());
+					metadataMap.put(request.getFilename(), hashlist);
+					if (twoPCsuccess + 1 < config.getNumMetadataServers()){
+						// not all followers updated
+						updatedFiles.add(request);
+					}
+					builder.setResult(WriteResult.Result.OK);
+					builder.setCurrentVersion(request.getVersion());
+				} else { // more than half of servers crashed
+					builder.setResult(WriteResult.Result.OLD_VERSION);
+                                	builder.setCurrentVersion(cur_version);
+				}
 			} else { /* not valid */
                         	builder.setResult(WriteResult.Result.OLD_VERSION);
                         	builder.setCurrentVersion(cur_version);
@@ -263,7 +398,11 @@ public final class MetadataStore {
 	@Override
     	public void restore(surfstore.SurfStoreBasic.Empty request,
         	io.grpc.stub.StreamObserver<surfstore.SurfStoreBasic.Empty> responseObserver) {
-    		
+    		logger.info("Restored");
+		crashed = false;
+		Empty response = Empty.newBuilder().build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();	
 	}
 
 	@Override
